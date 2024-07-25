262e063216b24cd65e71ffa371dc1016f1086eee
Mikołaj <niko.kosiorek@gmail.com>
2024-07-26 00:31:42 +0200

    Login method created

--- JwtWebApi/Controllers/AuthController.cs
+++ JwtWebApi/Controllers/AuthController.cs
@@ -19,7 +19,18 @@ namespace JwtWebApi.Controllers
             user.PasswordHash = passwordHash;
 
             return Ok(user);
+        }
 
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if (user.Username != request.Username)
+                return BadRequest("User not found");
+
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                return BadRequest("Wrong Password");
+
+            return Ok(user);
         }
     }
 }
